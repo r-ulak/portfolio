c3187d7c0b90ce0d0b795c3705f1ab69c6b48826
rojan-ulak <rojan.ulak@gmail.com>
2024-01-06 17:08:48 -0600

    portfolio updates and software skills to use iconify

--- src/components/softwareSkills/SoftwareSkill.js
+++ src/components/softwareSkills/SoftwareSkill.js
@@ -1,26 +1,36 @@
 import React from "react";
 import "./SoftwareSkill.scss";
-import {skillsSection} from "../../portfolio";
+import { skillsSection } from "../../portfolio";
 
 export default function SoftwareSkill() {
-  return (
-    <div>
-      <div className="software-skills-main-div">
-        <ul className="dev-icons">
-          {skillsSection.softwareSkills.map((skills, i) => {
-            return (
-              <li
-                key={i}
-                className="software-skill-inline"
-                name={skills.skillName}
-              >
-                <i className={skills.fontAwesomeClassname}></i>
-                <p>{skills.skillName}</p>
-              </li>
-            );
-          })}
-        </ul>
-      </div>
-    </div>
-  );
+    return (
+        <div>
+            <div className="software-skills-main-div">
+                <ul className="dev-icons">
+                    {skillsSection.softwareSkills.map((skills, i) => {
+                        return (
+                            <li className="software-skill-inline" name={skills.skillName}>
+                                {skills.fontAwesomeClassname && (
+                                    <span
+                                        className="iconify"
+                                        data-icon={skills.fontAwesomeClassname}
+                                        style={skills.style}
+                                        data-inline="false"
+                                    ></span>
+                                )}
+                                {!skills.fontAwesomeClassname && skills.imageSrc && (
+                                    <img
+                                        className="skill-image"
+                                        style={skills.style}
+                                        src={`${process.env.PUBLIC_URL}/skills/${skills.imageSrc}`}
+                                        alt={skills.skillName}
+                                    />
+                                )}
+                            </li>
+                        );
+                    })}
+                </ul>
+            </div>
+        </div>
+    );
 }
